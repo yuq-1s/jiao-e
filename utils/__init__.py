af400a9ac2fab3ece427ded1748efc25e1f08e92
yuq <zxdewr@sjtu.edu.cn>
2017-04-02 18:11:06 +0800

    Refactor: move spider into utils.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,27 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import os
 import sys
 import logging
 
 
+ASP_FORM_ID = ['__VIEWSTATE', '__VIEWSTATEGENERATOR', '__EVENTVALIDATION',
+               '__EVENTTARGET', '__EVENTARGUMENT', '__LASTFOCUS']
+
+
+EDU_URL = 'http://electsys.sjtu.edu.cn/edu/'
+LESSON_URL = EDU_URL+'lesson/viewLessonArrange.aspx'
+ELECT_URL = EDU_URL+'student/elect/'
+MAIN_URL = ELECT_URL+'electcheck.aspx?xklc=%d' % 1
+REMOVE_URL = ELECT_URL+'removeLessonFast.aspx'
+SUBMIT_URL = ELECT_URL+'electSubmit.aspx'
+RECOMMAND_URL = ELECT_URL+'RecommandTblOuter.aspx'
+SUMMER_URL = ELECT_URL + 'ShortSession.aspx'
+SUMMER_CHECK_URL = ELECT_URL+'ShortSessionCheck.aspx?xklc=%d' % 1
+
+
 # FIXME: Multiple call not checked, is that ok?
-def get_logger(module_name):
+def setup_logger(module_name):
     logger = logging.getLogger(module_name)
     logger.setLevel(logging.DEBUG)
 
@@ -33,8 +47,5 @@ def get_logger(module_name):
     logger.addHandler(ch)
     logger.addHandler(fh)
 
-    return logger
-
-
-logger = get_logger(__name__)
-
+class SessionOutdated(Exception):
+    pass
