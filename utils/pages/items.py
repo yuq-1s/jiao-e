041f6106f235b00e5cbf90b859d2833ce1f8f377
yuq <zxdewr@sjtu.edu.cn>
2017-04-06 16:33:15 +0800

    Fix: course time collectes only one period.

--- utils/pages/items.py
+++ utils/pages/items.py
@@ -10,8 +10,7 @@ class Course(Item):
                     output_processor=TakeFirst())
     teacher_job = Field(input_processor=MapCompose(remove_tags, str.strip),
                         output_processor=TakeFirst())
-    time = Field(input_processor=MapCompose(remove_tags, parse_time),
-                 output_processor=TakeFirst())
+    time = Field(input_processor=MapCompose(remove_tags, parse_time))
     cid = Field(input_processor=MapCompose(remove_tags, lambda s: re.search('\w{2}\d{3}', s).group(0)),
                 output_processor=TakeFirst())
     hours = Field(input_processor=MapCompose(remove_tags, int),
@@ -22,7 +21,9 @@ class Course(Item):
                        output_processor=TakeFirst())
     now_member = Field(input_processor=MapCompose(remove_tags, int),
                        output_processor=TakeFirst())
-    remark = Field(input_processor=MapCompose(str.strip))
+    remark = Field(input_processor=MapCompose(remove_tags, str.strip),
+                   output_processor=TakeFirst())
     course_type = Field(output_processor=TakeFirst())
     place = Field(input_processor=TakeFirst())
+    bsid = Field(input_processor=lambda s: int(s[0]), output_processor=TakeFirst())
 
